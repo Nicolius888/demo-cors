5a9187271eacdc6db4131bf0ec88accefe28c1a2
nicolius888 <nicola.edgardo@gmail.com>
2022-02-18 20:39:25 -0300

    let

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export function getData() {
   return async function (dispatch) {
-    console.log();
-    payload = await axios.get("https://demo-cors-backend.herokuapp.com/save");
+    let payload = await axios.get(
+      "https://demo-cors-backend.herokuapp.com/save"
+    );
     dispatch({
       type: "SET_DATA",
       payload,
