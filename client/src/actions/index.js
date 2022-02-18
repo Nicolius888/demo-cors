9fbd1d72be946d0036b9448179c31a566b08dab0
nicolius888 <nicola.edgardo@gmail.com>
2022-02-18 18:50:19 -0300

    add heroku backend link

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export function saveData(payload) {
   return async function () {
     console.log(payload);
-    await axios.post("http://localhost:3001/save", payload);
+    await axios.post("https://demo-cors-backend.herokuapp.com/save", payload);
   };
 }
