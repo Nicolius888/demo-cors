b1fb838470c6d535c225cd46352d2c4bf4b68907
nicolius888 <nicola.edgardo@gmail.com>
2022-02-18 18:36:18 -0300

    branch backend

--- client/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export function saveData(payload) {
-  return async function () {
-    console.log(payload);
-    await axios.post("http://localhost:3001/save", payload);
-  };
-}
